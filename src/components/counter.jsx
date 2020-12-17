504c90c4d1819e1772ed5dde57046b71ced5722a
sandeshdeshmukh1 <stdeshmukh@mitaoe.ac.in>
2020-12-17 12:39:01 +0530

    design change

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -12,16 +12,16 @@ class Counter extends Component {
             <div style={this.styles}>
                 <span className={this.handleColorFormatter()}>{this.props.counter.value}</span>
                 <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-primary m-2">+</button>
-                <button onClick={() => this.props.onDecrement(this.props.counter)} className="badge-lg badge-info m-2" disabled={this.props.counter.value === 0 ? "disabled" : ""}>-</button>
+                <button onClick={() => this.props.onDecrement(this.props.counter)} className="btn-lg btn-info m-2" disabled={this.props.counter.value === 0 ? "disabled" : ""}>-</button>
                 <button className="btn btn-danger m-2" onClick={() => this.props.onDelete(this.props.counter.id)}>0</button>
             </div>);
     }
     handleColorFormatter() {
-        let classes = "badge m-2 badge-";
+        let classes = "btn m-2 btn-";
         return classes += this.props.counter.value === 0 ? "danger" : "warning";
     }
     handeldeletebtn() {
-        let classes = "badge badge-success  ";
+        let classes = "btn btn-success  ";
         return classes += this.props.counter.value === 0 ? " disabled" : "m-2";
 
     }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,6 +6,7 @@ class Navbar extends Component {
         return <div>
             <nav class="navbar navbar-light bg-light">
                 <button className="badge badge-primary badge-lg">{this.props.totalCounters}</button>
+                <p>NavBar</p>
             </nav>
         </div>;
     }
